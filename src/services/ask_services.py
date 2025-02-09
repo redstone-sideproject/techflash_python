939da795c3ef78610cf83e8280d5a771c6941cfb
k-redstone <che0618@naver.com>
2025-02-10 03:40:09 +0900

    feat: 환경변수 세팅

--- src/services/ask_services.py
+++ src/services/ask_services.py
@@ -1,5 +1,9 @@
-def generate_response(question: str) -> dict:
+from src.settings import Settings
 
+settings =Settings()
+
+def generate_response(question: str) -> dict:
+    print(settings.GCP_LOCATION)
     dummy_response ={
         "question": question,
         "answer": "Virtual DOM은 실제 DOM을 변경하기 전에 가상으로 상태를 관리하는 개념입니다.",
